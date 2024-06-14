2b8168c7f54bf3ceb5637a24e707a1d1831962e3
ROMA2 <romasecure84@yandex.ru>
2024-06-15 00:14:43 +0400

    basic command of selenium created

---
+++ src/test/java/myFirstClass.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class myFirstClass {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        driver.navigate().to("https://www.udemy.com");
+        Thread.sleep(5000);
+        driver.navigate().back();
+        String googleUrl = driver.getCurrentUrl();
+        System.out.println(googleUrl);
+        driver.navigate().refresh();
+        driver.navigate().forward();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
