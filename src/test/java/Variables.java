839f760fc12a5bfd7d97f8a6a62e6c2ef9816adc
ROMA2 <romasecure84@yandex.ru>
2024-06-17 23:43:58 +0400

    Initial Variables lessons added

---
+++ src/test/java/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Initialization & Declaration
+        String companyName = "Shern";
+
+        //Declaration
+        byte age;
+        //Initialization
+        age = 20;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        short height = 180;
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+    }
+}
--- src/test/java/myFirstClass.java
+++ src/test/java/myFirstClass.java
@@ -5,18 +5,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class myFirstClass {
     public static void main(String[] args) throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.google.com");
-        driver.navigate().to("https://www.udemy.com");
-        Thread.sleep(5000);
-        driver.navigate().back();
-        String googleUrl = driver.getCurrentUrl();
-        System.out.println(googleUrl);
-        driver.navigate().refresh();
-        driver.navigate().forward();
-        Thread.sleep(5000);
-        driver.quit();
+        System.out.println("Hello world!");
+        System.out.print("Hello world!");
+        System.out.print("Hi");
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver=new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://www.google.com");
+//        driver.navigate().to("https://www.udemy.com");
+//        Thread.sleep(5000);
+//        driver.navigate().back();
+//        String googleUrl = driver.getCurrentUrl();
+//        System.out.println(googleUrl);
+//        driver.navigate().refresh();
+//        driver.navigate().forward();
+//        Thread.sleep(5000);
+//        driver.quit();
     }
 }
