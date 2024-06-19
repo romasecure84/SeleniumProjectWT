c3be1226e635d60a6a5f462b5ea1751ebb0a958d
ROMA2 <romasecure84@yandex.ru>
2024-06-19 23:32:52 +0400

    Testing WhileLoop lesson added

---
+++ src/test/java/WhileLoop.java
@@ -0,0 +1,22 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+//        int x = 3;
+//        while (x <= 10){
+//            System.out.println(x);
+//            x++;
+//        }
+        /*
+        int i = 1;
+        do {
+            System.out.println("The value of i is: " + i);
+            i++;
+        } while (i <= 10);
+        */
+         int i = 1;
+         while (i <= 10){
+             System.out.println("The value of i is: " + i);
+             i++;
+         }
+
+    }
+}
